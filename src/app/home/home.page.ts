76429aa67c1f9fe4ef1ad2be0c10544c7ba81343
Your Name <you@example.com>
2024-10-05 17:03:18 +0300

    feat: add new

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+interface BookType {
+  book_name:string
+  book_author:string
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-l:string ="Agapkina Polina Dmitrievna"
+  list_name: string="My books"
+  input_Name: string="--"
+  input_Author: string="--"
+  input_value: string="--"
+  set_value: string="__"
+  books: BookType[]=[]
+  set_my_value(){
+    this.books.push({book_name: this.input_Name, book_author: this.input_Author})
+    this.set_value=this.input_value
+  }
   constructor() {}
-
 }
+
